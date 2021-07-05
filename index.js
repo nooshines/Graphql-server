f3fe95a05e712f9257c785e1ceb9b1093a92773b
nooshines <nooshin_es26413@yahoo.com>
2021-07-05 21:25:08 +1000

    node-zipfile

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const fs = require("fs");
 
+const archiver = require('archiver');
+
 const path =require("path")
 
 const multer =require("multer")
@@ -44,22 +46,64 @@ const storage = multer.diskStorage({
   const compressfilesupload = multer({ storage: storage,limits:{fileSize:maxSize}});
 
 app.post("/compressfiles",compressfilesupload.array("file",100),(req,res)=>{
-    const zip = new JSZip();
-if(req.files){
-    req.files.forEach((file,i) => {
-        console.log("file.path",file.path)
-        zip.file(`file-${i}.jpg`, file.path);
-    })
+    // const zip = new JSZip();
+// if(req.files){
+//     req.files.forEach((file,i) => {
+//         console.log("file.path",file.path)
+//         zip.file(`file-${i}.jpg`, file.path);
+//     })
 
-    zip.generateAsync({ type: "nodebuffer"}).then((zipFile) => {
-        const currentDate = new Date().getTime();
-        const fileName = `combined-${currentDate}.zip`;
-        return FileSaver.saveAs(zipFile, fileName);
-      });
-   }
+//     zip.generateAsync({ type: "nodebuffer"}).then((zipFile) => {
+//         const currentDate = new Date().getTime();
+//         const fileName = `combined-${currentDate}.zip`;
+//         return FileSaver.saveAs(zipFile, fileName);
+//       });
+//    }
 
 
-})
+
+const output = fs.createWriteStream(__dirname + '/public/example.zip');
+const archive = archiver('zip', {
+  zlib: { level: 9 } // Sets the compression level.
+});
+// listen for all archive data to be written
+// 'close' event is fired only when a file descriptor is involved
+output.on('close', function() {
+  console.log(archive.pointer() + ' total bytes');
+  console.log('archiver has been finalized and the output file descriptor has closed.');
+});
+// This event is fired when the data source is drained no matter what was the data source.
+// It is not part of this library but rather from the NodeJS Stream API.
+// @see: https://nodejs.org/api/stream.html#stream_event_end
+output.on('end', function() {
+  console.log('Data has been drained');
+});
+// good practice to catch warnings (ie stat failures and other non-blocking errors)
+archive.on('warning', function(err) {
+  if (err.code === 'ENOENT') {
+    // log warning
+  } else {
+    // throw error
+    throw err;
+  }
+});
+// good practice to catch this error explicitly
+archive.on('error', function(err) {
+  throw err;
+});
+
+// pipe archive data to the file
+archive.pipe(output);
+
+const buffer3 = Buffer.from('buff it!');
+
+archive.directory('public/uploads', false);
+
+archive.finalize();
+
+archive.pipe(res);
+
+});
 
 
 
