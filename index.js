4b7257a6d949f3d0af58a50b0e4cde688eacc050
nooshines <nooshin_es26413@yahoo.com>
2021-07-05 21:50:50 +1000

    added archiver

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const express = require("express");
-
 const fs = require("fs");
-
 const archiver = require('archiver');
-
 const path =require("path")
-
 const multer =require("multer")
-
 const FileSaver = require('file-saver');
-
 const JSZip = require("jszip");
-
 const app = express()
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/index.html");
 })
-
 
 const dir = "public";
 const subDir = "public/uploads"
@@ -45,7 +37,10 @@ const storage = multer.diskStorage({
 
   const compressfilesupload = multer({ storage: storage,limits:{fileSize:maxSize}});
 
-app.post("/compressfiles",compressfilesupload.array("file",100),(req,res)=>{
+app.post("/compressfiles",compressfilesupload.array("file",50),(req,res)=>{
+
+    // *********************** JSZIP *********************** //
+
     // const zip = new JSZip();
 // if(req.files){
 //     req.files.forEach((file,i) => {
@@ -61,50 +56,19 @@ app.post("/compressfiles",compressfilesupload.array("file",100),(req,res)=>{
 //    }
 
 
-
+  // *********************** Archiver *********************** //
+  
 const output = fs.createWriteStream(__dirname + '/public/example.zip');
 const archive = archiver('zip', {
   zlib: { level: 9 } // Sets the compression level.
 });
-// listen for all archive data to be written
-// 'close' event is fired only when a file descriptor is involved
-output.on('close', function() {
-  console.log(archive.pointer() + ' total bytes');
-  console.log('archiver has been finalized and the output file descriptor has closed.');
-});
-// This event is fired when the data source is drained no matter what was the data source.
-// It is not part of this library but rather from the NodeJS Stream API.
-// @see: https://nodejs.org/api/stream.html#stream_event_end
-output.on('end', function() {
-  console.log('Data has been drained');
-});
-// good practice to catch warnings (ie stat failures and other non-blocking errors)
-archive.on('warning', function(err) {
-  if (err.code === 'ENOENT') {
-    // log warning
-  } else {
-    // throw error
-    throw err;
-  }
-});
-// good practice to catch this error explicitly
-archive.on('error', function(err) {
-  throw err;
-});
 
-// pipe archive data to the file
 archive.pipe(output);
-
-const buffer3 = Buffer.from('buff it!');
-
 archive.directory('public/uploads', false);
-
 archive.finalize();
-
 archive.pipe(res);
 
 });
-
 
 
 app.listen(4000,()=>{
